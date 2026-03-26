db530f0b92a0b816e9b314887904bcad78fbffcb
Metaxha <barkinarik50@gmail.com>
2026-03-27 02:38:23 +0300

    vite config düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Grafik kütüphanesini ana koddan ayırıyoruz. 
-          // Sadece grafik sayfasına girilince yüklenecek.
-          vendor_charts: ['react-apexcharts', 'apexcharts'],
-          
-          // React'in çekirdek dosyalarını ayrı paketliyoruz
-          vendor_react: ['react', 'react-dom']
-        }
-      }
-    }
-  }
 })
