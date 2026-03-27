0080e7e072780f31f131d6259c330724aab7d847
Metaxha <barkinarik50@gmail.com>
2026-03-27 03:45:50 +0300

    geri aldık

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    cssInjectedByJsPlugin()
   ],
-  build: {
-    // Kod bölme (Code Splitting) ve küçültme ayarları
-    minify: 'terser', 
-    terserOptions: {
-      compress: {
-        drop_console: true, // Production'da console logları siler, JS boyutunu düşürür
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        // Vendor (kütüphane) kodlarını ayrı dosyalara böler, cache avantajı sağlar
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-    // CSS dosyalarını tek bir parça yerine modüler bırakmak istersen:
-    cssCodeSplit: true,
-    assetsInlineLimit: 4096, // 4kb altındaki dosyaları base64 yaparak istek sayısını azaltır
-  },
 })
