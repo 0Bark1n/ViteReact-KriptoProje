272b65213b0d1263e3758ec84fef195564e53ba3
Metaxha <barkinarik50@gmail.com>
2026-03-27 18:13:14 +0300

    son lighthouse ayarları.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cssInjectedByJsPlugin() // CSS'i tekrar içeri gömüyoruz çünkü dosyan çok küçük (5kb)
-  ],
-  build: {
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Dosyaları bölme, tek parça (bundle) yap
+  plugins: [react()],
+  
+  // SASS UYARILARINI SUSTURDUĞUMUZ YER (İşte bu kadar basit!)
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import'],
       },
     },
   },
-})
+
+  build: {
+    chunkSizeWarningLimit: 800, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) return 'vendor-react';
+          if (id.includes('node_modules/apexcharts') || id.includes('node_modules/react-apexcharts')) return 'vendor-charts';
+          if (id.includes('node_modules')) return 'vendor';
+        }
+      }
+    }
+  }
+});
